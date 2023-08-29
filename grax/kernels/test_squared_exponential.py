7e9d694cfc6305893169e3746f50eb0af1e9558e
Matt Hoffman <mhoffman@gmail.com>
2023-08-29 23:44:45 +0100

    Remove unittest; add kernel call/golden tests.

--- grax/kernels/test_squared_exponential.py
+++ grax/kernels/test_squared_exponential.py
@@ -2,55 +2,64 @@
 """
 
 import numpy as np
-import unittest
+import pickle
+import pytest
 
 from grax.kernels.squared_exponential import SquaredExponential
+from os import path
 
 
-class TestSquaredExponential(unittest.TestCase):
+def test_init():
+  with pytest.raises(ValueError):
+    SquaredExponential(1, 1)
 
-  def test_init(self):
-    with self.assertRaises(ValueError):
-      SquaredExponential(1, 1)
+  with pytest.raises(ValueError):
+    SquaredExponential(1, 1, dim=-1)
+    
+  with pytest.raises(ValueError):
+    SquaredExponential(1, [1, 1], dim=1)
 
-    with self.assertRaises(ValueError):
-      SquaredExponential(1, 1, dim=-1)
-      
-    with self.assertRaises(ValueError):
-      SquaredExponential(1, [1, 1], dim=1)
 
-  def test_repr(self):
-    self.assertEqual(
-      repr(SquaredExponential(1, 1, dim=1, dtype='float16')),
-      'SquaredExponential(rho=1.0, ell=1.0, dim=1, dtype=float16)')
+def test_repr():
+  result = repr(SquaredExponential(1, 1, dim=1, dtype='float16'))
+  desired = 'SquaredExponential(rho=1.0, ell=1.0, dim=1, dtype=float16)'
+  assert result == desired
 
-    self.assertEqual(
-      repr(SquaredExponential(1, [1, 1])),
-      'SquaredExponential(rho=1.0, ell=[1.0, 1.0])')
+  result = repr(SquaredExponential(1, [1, 1]))
+  desired = 'SquaredExponential(rho=1.0, ell=[1.0, 1.0])'
+  assert result == desired
 
-  def test_dim(self):
-    kernel1 = SquaredExponential(1, 1, dim=1)
-    kernel2 = SquaredExponential(1, [1, 1])
 
-    self.assertEqual(kernel1.dim, 1)
-    self.assertEqual(kernel2.dim, 2)
+def test_dim():
+  assert SquaredExponential(1, 1, dim=1).dim == 1
+  assert SquaredExponential(1, [1, 1]).dim == 2
 
-  def test_call(self):
-    n, m = 8, 5
-    kernels = [
-        SquaredExponential(1, 1, dim=1),
-        SquaredExponential(1, 1, dim=2),
-        SquaredExponential(1, [1, 2])
-    ]
 
-    for kernel in kernels:
-      x1 = np.linspace(0, 1, n*kernel.dim).reshape(n, -1)
-      x2 = np.linspace(0, 1, m*kernel.dim).reshape(m, -1)
+def test_call():
+  n, m = 8, 5
+  kernels = [
+      SquaredExponential(1, 1, dim=1),
+      SquaredExponential(1, 1, dim=2),
+      SquaredExponential(1, [1, 2])
+  ]
 
-      self.assertEqual(kernel(x1, x2).shape, (n, m))
-      self.assertEqual(kernel(x2, x1).shape, (m, n))
-      self.assertEqual(kernel(x1).shape, (n, n))
-      self.assertEqual(kernel(x1, diag=True).shape, (n,))
+  for kernel in kernels:
+    x1 = np.linspace(0, 1, n*kernel.dim).reshape(n, -1)
+    x2 = np.linspace(0, 1, m*kernel.dim).reshape(m, -1)
 
-      with self.assertRaises(ValueError):
-        kernel(x1, x2, diag=True)
+    assert kernel(x1, x2).shape == (n, m)
+    assert kernel(x2, x1).shape == (m, n)
+    assert kernel(x1).shape == (n, n)
+    assert kernel(x1, diag=True).shape == (n,)
+
+    with pytest.raises(ValueError):
+      kernel(x1, x2, diag=True)
+
+
+def test_goldens():
+  with open(path.splitext(__file__)[0] + '.golden', 'rb') as f:
+    goldens = pickle.load(f)
+
+  for class_kwargs, call_kwargs, desired in goldens:
+    result = SquaredExponential(**class_kwargs)(**call_kwargs)
+    assert np.allclose(result, desired)
