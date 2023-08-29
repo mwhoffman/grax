2171ccce41892fa55c297bff6086241714be97da
Matt Hoffman <mhoffman@gmail.com>
2023-08-29 02:36:12 +0100

    Update se kernel tests.

--- grax/kernels/test_squared_exponential.py
+++ grax/kernels/test_squared_exponential.py
@@ -1,8 +1,36 @@
 """Tests for the squared exponential kernel.
 """
 
-from grax.kernels import squared_exponential
+import unittest
+
+from grax.kernels.squared_exponential import SquaredExponential
 
 
-def test_kernel():
-  kernel = squared_exponential.SquaredExponential(1, 1, dim=1)
+class TestSquaredExponential(unittest.TestCase):
+
+  def test_init(self):
+    with self.assertRaises(ValueError):
+      SquaredExponential(1, 1)
+
+    with self.assertRaises(ValueError):
+      SquaredExponential(1, 1, dim=-1)
+      
+    with self.assertRaises(ValueError):
+      SquaredExponential(1, [1, 1], dim=1)
+
+  def test_repr(self):
+    self.assertEqual(
+      repr(SquaredExponential(1, 1, dim=1, dtype='float16')),
+      'SquaredExponential(rho=1.0, ell=1.0, dim=1, dtype=float16)')
+
+    self.assertEqual(
+      repr(SquaredExponential(1, [1, 1])),
+      'SquaredExponential(rho=1.0, ell=[1.0, 1.0])')
+
+  def test_dim(self):
+    kernel1 = SquaredExponential(1, 1, dim=1)
+    kernel2 = SquaredExponential(1, [1, 1])
+
+    self.assertEqual(kernel1.dim, 1)
+    self.assertEqual(kernel2.dim, 2)
+
